a6b7c35d51ecac0f8bb05351b8b7c34d73569ccb
Adarsh Gupta <adarshgupta6560@gmail.com>
2020-11-17 13:56:48 +0600

    commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import {Switch,Router, Route} from 'react-router-dom';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Details from "./components/Details";
+import Cart from "./components/Cart/Cart";
+import Default from "./components/Default";
+import ProductList from './components/ProductList';
+import Modal from './components/Modal'
+
+
+
+class App extends Component {
+  render()  {
+      return (
+      <React.Fragment>
+ 
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart} />
+          <Route component={Default}/>
+        </Switch>
+        <Modal />
+      </React.Fragment>
+      );
+    }
+}
+
+export default App;
